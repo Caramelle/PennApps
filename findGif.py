964194b2248fb558d7f9f3e0baf2957a56f590b9
caramelle <andreea.cucu@gmail.com>
2017-01-21 12:23:01 -0500

    added slide scraper and gif finder

---
+++ findGif.py
@@ -0,0 +1,10 @@
+import giphypop
+
+g = giphypop.Giphy()
+gifs = []
+keyword = 'coffee'
+for i in range (0,9):
+	image = giphypop.translate(keyword)
+	gifs.append(image.fixed_height.downsampled.url)
+	del image
+print gifs
