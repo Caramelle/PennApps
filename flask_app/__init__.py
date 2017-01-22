cf369ee62e43a6beec798cec90f09d63343a2841
Arthur <arthur.wilcke@gmail.com>
2017-01-22 03:58:31 -0500

    heroku

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///gifdb'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secreeeeet_key'
 db = SQLAlchemy(app)
 db.create_all()
