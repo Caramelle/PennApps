1c51ed778c7a24dd14dcbe3966eb23b86b71992d
caramelle <andreea.cucu@gmail.com>
2017-01-21 19:30:06 -0500

    permissions not working properly. otherwise it should be fine

--- findText.py
+++ findText.py
@@ -8,21 +8,28 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
+
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
 
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/slides.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/presentations.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Slides API Python Quickstart'
 
-def url_to_presenationid(url)
+
+def url_to_presenationId(url):
+    parts = url.split('/')
+    return parts[5]
+
 
 def get_credentials():
+
+    # If modifying these scopes, delete your previously saved credentials
+    # at ~/.credentials/slides.googleapis.com-python-quickstart.json
+    SCOPES = 'https://www.googleapis.com/auth/presentations'
+    CLIENT_SECRET_FILE = 'client_secret.json'
+    APPLICATION_NAME = 'Google Slides API Python Quickstart'
+
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -48,7 +55,7 @@ def find_text(url):
     service = discovery.build('slides', 'v1', http=http)
     presentationText = []
 
-    presentationId = url_to_presenationid(url)
+    presentationId = url_to_presenationId(url)
     presentation = service.presentations().get(
         presentationId=presentationId).execute()
     slides = presentation.get('slides')
@@ -64,11 +71,47 @@ def find_text(url):
                     content = writing['content']
                     with open("slideTexts.txt", "a") as myfile:
                         myfile.write(content)
-        # for element in elements:
-        #     print json.dumps(element, sort_keys=True, indent=4)
-    #     if slides.
-        #        print('- Slide #{} contains {} elements.'.format(i + 1,
-        #    len(slide.get('pageElements'))))
 
-if __name__ == '__main__':
-    main()
+def add_image(url, image_url, page_id):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('slides', 'v1', http=http)
+
+    presentationId = url_to_presenationId(url)
+    requests = []
+    image_id = 'MyImage_01'
+    emu4M = {
+        'magnitude': 4000000,
+        'unit': 'EMU'
+    }
+    requests.append({
+        'createImage': {
+            'objectId': image_id,
+            'url': image_url,
+            'elementProperties': {
+                'pageObjectId': page_id,
+                'size': {
+                    'height': emu4M,
+                    'width': emu4M
+                },
+                'transform': {
+                    'scaleX': 1,
+                    'scaleY': 1,
+                    'translateX': 100000,
+                    'translateY': 100000,
+                    'unit': 'EMU'
+                }
+            }
+        }
+    })
+
+    # Execute the request.
+    body = {
+        'requests': requests
+    }
+    response = service.presentations().batchUpdate(presentationId=presentationId,
+                                                          body=body).execute()
+    create_image_response = response.get('replies')[0].get('createImage')
+    print('Created image with ID: {0}'.format(create_image_response.get('objectId')))
+
+add_image("https://docs.google.com/presentation/d/1BUUh7a92Smqvis_ht0XZZl9BqwKfKkkpUTn9F9Fo8no/edit", "http://media0.giphy.com/media/26uffErnoIpeQ3PmU/200_d.gif", 0)
