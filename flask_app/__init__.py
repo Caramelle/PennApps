5844030bcf7940deae8b7038e16fe31023d19a12
Arthur <arthur.wilcke@gmail.com>
2017-01-21 23:29:30 -0500

    without flaskheroku

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///gifdb'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.secret_key = 'secreeeeet_key'
-heroku = Heroku(app)
 db = SQLAlchemy(app)
 db.create_all()
+
 
 import flask_app.views
--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-from flask_app import app, db
+from flask_app import app
 
 if __name__ == '__main__':
-    # app.debug = True
-    db.create_all()
-    db.configure_mappers()
-    app.run()
+   app.run()
